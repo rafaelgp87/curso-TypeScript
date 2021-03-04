cce2bf51df21d3d6e0f0b72a6c54286199f90278
Rafael Gutiérrez <rafaelgp87@gmail.com>
2021-03-03 19:58:20 -0600

    Nuevo archivo 02-arr-obj-interface.ts

---
+++ src/ejercicios/02-arr-obj-interface.ts
@@ -0,0 +1,26 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+//let habilidades: (string | boolean | number)[] = ['Bash','Counter','Healing', 100];
+//habilidades.push(true)
+
+let habilidades: string[] = ['Bash','Counter','Healing'];
+
+interface Personaje {
+    nombre: string;
+    hp: number;
+    habilidades: string[];
+    puebloNatal?: string;
+}
+
+const personaje: Personaje = {
+    nombre: 'Strider',
+    hp: 100,
+    habilidades: ['Bash','Counter','Healing']
+}
+
+personaje.puebloNatal = 'Pueblo Paleta';
+
+console.table ( personaje );
