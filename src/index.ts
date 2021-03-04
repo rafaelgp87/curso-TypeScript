4b344f1fd6823cae8db86fa86a25dfa9a4929b4c
Rafael Gutiérrez <rafaelgp87@gmail.com>
2021-03-03 19:43:59 -0600

    Nuevo archivo 01-tipos-basicos.ts

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,4 @@
     ===== Código de TypeScript =====
 */
 
-interface Reproductor {
-    volumen: number;
-    segundo: number;
-    cancion: string;
-    detalles: Detalles
-}
 
-interface Detalles {
-    autor: string;
-    anio: number;
-}
-
-const reproductor: Reproductor = {
-    volumen: 90,
-    segundo: 36,
-    cancion: 'Mess',
-    detalles: {
-        autor: 'Ed Sheeran',
-        anio: 2015
-    }
-}
