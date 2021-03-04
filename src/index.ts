05db203d7537cb8bb21599549b104f364383cc3f
Rafael Gutiérrez <rafaelgp87@gmail.com>
2021-03-03 19:17:29 -0600

    Primer commit desde Windows 10.

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles
+}
+
+interface Detalles {
+    autor: string;
+    anio: number;
+}
+
+const reproductor: Reproductor = {
+    volumen: 90,
+    segundo: 36,
+    cancion: 'Mess',
+    detalles: {
+        autor: 'Ed Sheeran',
+        anio: 2015
+    }
+}
