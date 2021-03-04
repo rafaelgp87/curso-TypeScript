1aedfe82fb6e9aaec87563277495ba3ead86ea5e
Rafael Gutiérrez <rafaelgp87@gmail.com>
2021-03-03 22:22:38 -0600

    Nuevo archivo 11-optional-chaining.ts

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,5 @@
     ===== Código de TypeScript =====
 */
 
-function classDecorator<T extends { new (...args: any[]): {} }>(
-    constructor: T
-  ) {
-    return class extends constructor {
-      newProperty = "new property";
-      hello = "override";
-    };
-  }
 
-@classDecorator
-class MiSuperClase {
-    public miPopiedad: string = 'ABC123';
 
-    imprimir() {
-        console.log('Hola Mundo');
-    }
-}
-
-console.log( MiSuperClase );
-
-const miClase = new MiSuperClase();
-
-console.log( miClase.miPopiedad );
